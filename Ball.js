13dbd883723f388a5b344a1f89673fef4c993966
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-01-10 10:28:05 +0000

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,27 @@
+class Ball {
+    constructor(x,y,width,height){
+        var options = {
+           frictionAir:0.005,
+           density:1
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        strokeWeight(3);
+        stroke("maroon");
+        fill("cyan");
+        ellipse(0,0,this.width,this.height);
+        pop();
+    }
+
+
+
+}
